492aa19550a56e646605ac106ecc2e5b176b9429
behom002 <benedict.homuth@gmail.com>
2018-12-22 09:24:30 -0800

    Added a find function to find a number in a tree

--- BinaryTree.java
+++ BinaryTree.java
@@ -5,19 +5,19 @@ public class BinaryTree{
     root = null;
   }
 
-  public static Node getNode(BinaryTree aTree){
-    return aTree.root;
-  }
-
   public void traverse(){
     this.root.visit();
+  }
+
+  public void find(int value){
+    this.root.search(value);
   }
 
   public void addValue(int val){
     Node aNode = new Node(val);
     if(this.root == null){
       this.root = aNode;
-    }else{
+    } else{
       this.root.addNode(aNode);
     }
   }
@@ -26,14 +26,12 @@ public class BinaryTree{
     BinaryTree bTree = new BinaryTree();
     BinaryTree secondTree = new BinaryTree();
 
-
-    bTree.addValue(2);
-    bTree.addValue(3);
-    bTree.addValue(8);
-    bTree.addValue(7);
+    for(int i = 0; i < 10; i++){
+      bTree.addValue(i);
+    }
 
     bTree.traverse();
-
+    bTree.find(4);
   }
 
 }
